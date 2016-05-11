bb7757549be9f01ff9065f7b73cd524ee71d91cd
Sylvain <sylvain.doignon@gmail.com>
2016-05-11 15:32:36 +0200

    hey

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // Init modules
+var fs = require('fs');
 var express = require('express');
+var sharejss = require("sharejss"); 
+var bodyParser = require("body-parser");
 var app = express();
 
-// Config (defnied in package.json)
+// Config (definied in package.json)
 app.locals.config = require('./config.json');
+
+// Custom shared functions
+app.locals.myFunctions = sharejss("./static/js/shared.js"); 
 
 // Public dir
 app.use(express.static('static'));
@@ -11,6 +17,10 @@ app.use(express.static('static'));
 // Use Pug template
 app.set('view engine', 'pug');
 app.set('views', 'views');
+
+// Parse JSON in POST
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Define routes
 app
@@ -28,7 +38,28 @@ app
 })
 .get('*', function(req, res){
     res.render('404');
-});
+})
+.post('/get-programmation', function (req, res) {
+    if (req.body.action == "last") {
+      if (req.body.numberLines < 0)
+        var numberLines = 1;
+      var d = new Date();
+      var file = d.getFullYear() + "-" + app.locals.myFunctions.twoDigitsNumber(d.getMonth()+1) + "-" + d.getDate();
+      file += "_airplay.log"
+    } else
+      res.status(400);
+    file = app.locals.config.AIRPLAY_FOLDER + file;
+    fs.readFile(file, 'utf-8', function(err, data) {
+        if (err) throw err;
+        var lines = data.trim().split('\n');
+        if (req.body.action == "last") {
+        var lastLine = lines.slice(-numberLines)[0];
+        console.log(lastLine);  
+        } else {
+          true;
+        }
+    });
+})
 
 // Launch the server
 app.listen(app.locals.config.PORT, function () {
--- static/js/main.js
+++ static/js/main.js
@@ -11,11 +11,10 @@ function setDynamicColors(nb) {
   var color = "hsl(" + tint + ", 60%, 75% )";
   var colorActive = "hsl(" + tint + ", 60%, 60% )";
   var colorHover = "hsl(" + tint + ", 60%, 65% )";
-  var rule = "aside a.button { background-color: " + color + "}"
+  var rule = "aside a.button, .title-bar { background-color: " + color + "}"
   var ruleActive = "aside a.button.active { background-color: " + colorActive + "}";
   var ruleHover = "aside a.button:hover { background-color: " + colorHover + "}";
   var ruleProgressBar = "#nprogress .bar { background: " + color + " !important}";
-  console.log(ruleProgressBar)
   var sheet = window.document.styleSheets[0];
   var insertAt = sheet.cssRules.length;
   sheet.insertRule(rule, insertAt);
@@ -59,3 +58,22 @@ function refreshActivePageInMenu() {
 }
 refreshActivePageInMenu();
 
+
+// Set defaults select in menu
+var selector = "#searchDay option[value='" + returnDate() + "']";
+$(selector).attr("selected", "selected").text("Aujourd'hui");
+selector = "#searchHour option[value^='" + (new Date).getHours() + "']";
+$(selector).first().attr("selected", "selected");
+
+
+// Set last
+$.ajax({
+    url: "/get-programmation",
+    data : { 
+      "action" : "last" 
+    },
+    dataType: "application/json",
+    method: "POST"
+}).success(function(data) {
+    console.log(data)
+});
